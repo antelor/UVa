62cba5ef792caa7b66100470765a4d63c52b49ed
Ramiro Antelo <ramiro.antelo4@gmail.com>
2018-10-28 19:06:14 -0300

    pushee main equivocado 10114

--- 10114 - Loansome Car Buyer/10114.cpp
+++ 10114 - Loansome Car Buyer/10114.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <stdio.h>
 
 using namespace std;
 
+float records[110];
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int D, N;
+    float P, A;
+    while( scanf("%i %f %f %i", &D, &P, &A, &N) && D>=0){
+        double carvalue = A + P;
+        double owes = A;
+
+        int index;
+        float perc;
+        for(int i=0; i<N; i++){
+            scanf("%i", &index);
+            scanf("%f", &perc);
+
+
+            for(int j=index; j<D; j++){
+                records[j]=perc;
+            }
+        }
+
+        double amount = A/D;
+
+        int month=0;
+
+        carvalue = carvalue * (1-records[month]);
+
+        while(carvalue < owes){
+            month++;
+
+            owes -= amount;
+            carvalue -= carvalue*records[month];
+        }
+
+        (month==1) ? printf("1 month\n") : printf("%i months\n", month);
+    }
     return 0;
 }
